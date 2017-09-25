f3c8118a983daf63cdd794965fffead4a5e8b129
Kendal Gates <leoneusg@yahoo.com>
2017-09-25 19:11:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 kittens = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendKitten(name) {
-  kittens.push(name)
+  kittens.push('Ralph')
   return kittens
 }
